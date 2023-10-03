9714e58d7e5f073f7324eb7c6a99a3bd11cef57f
Ale <146327980+dromaeo@users.noreply.github.com>
2023-10-03 17:51:27 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -1,4 +1,4 @@
-# Comment - 42 is the answer to everything
+# Comment 42 is the answer to everything
 
 # R as a calculator
 2+3
@@ -12,3 +12,7 @@ f^2
 
 #Array
 s<- c(10, 20, 30, 50)
+c<-c(100, 500, 600, 1000)
+plot (c, s, xlab="n people", yab="microplastics", pch=19, cex=2, col="blue")
+
+#Package: sp, spatial data
