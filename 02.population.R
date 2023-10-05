678ddade8b992031c4505094cb350f92bcd03428
dromaeo <146327980+dromaeo@users.noreply.github.com>
2023-10-05 17:38:45 +0200

    Update 02.population.R

--- 02.population.R
+++ 02.population.R
@@ -2,5 +2,10 @@
 # Surviving as much as we can as long as we have food, Numer in time
 # worldometer
 
-install.packages("spatstat")
+# install.packages("spatstat")
+# install.packages("terra")
 library(spatstat)
+library(terra)
+
+bei 
+plot(bei, pch=19, cex=.5) 
