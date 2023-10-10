f9b9f2eb181d046074c820566ea6712eef7c781a
dromaeo <146327980+dromaeo@users.noreply.github.com>
2023-10-10 16:43:46 +0200

    Update 02.population.R

--- 02.population.R
+++ 02.population.R
@@ -6,10 +6,13 @@
 # install.packages("terra")
 library(spatstat)
 library(terra)
+# Do not save locally R, data from outside
+# " when outside R, the world is scary, when inside no need
 
 bei 
 plot(bei, pch=19, cex=.5) 
 # we want to understand why there are forests there
+# data clustered is an island
 
 bei.extra
 # It contains 2 levels of maps: a raster each (aratro each pixel) is a map, elevation and grad
@@ -18,9 +21,10 @@ plot(bei.extra)
 plot(bei.extra$elev)
 elevation<-bei.extra$elev
  #6th or 7th person will have same birthday as another. cumulative population
-
 plot(bei.extra[[1]])
+
 density_map <- density(bei)
+# passing from points to a continuous surface
 
 par(mfrow=c(2,1))
 plot(bei.extra[[1]])
