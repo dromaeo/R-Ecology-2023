00a884a758454065910ce5d7a2dfc16485b47a22
dromaeo <146327980+dromaeo@users.noreply.github.com>
2023-11-02 16:58:00 +0100

    Update 04.remotesensing.R

--- 04.remotesensing.R
+++ 04.remotesensing.R
@@ -1,35 +1,23 @@
 # This is a script to visualize satellite data
 
-library(devtools) # packages in R are also called libraries
-
+library(devtools)
 # install the imageRy package from GitHub
 devtools::install_github("ducciorocchini/imageRy")
 
 library(imageRy)
 library(terra)
 
-# simple operation
-10 + 10
-
-# this is an object
 duccio <- 10 + 10
-duccio
-
-# objects
+duccio # objects
 adam <- 5 + 3
 duccio + adam
-
 # arrays
 diameter <- c(100, 10, 50, 20, 15)
 iron <- c(10, 1000, 20, 700, 900)
 
 # a function
-plot(iron, diameter) # iron and diameter are arguments!
-plot(iron, diameter, pch=19) # https://www.google.com/search?client=ubuntu-sn&hs=Ssn&sca_esv=564367827&channel=fs&sxsrf=AB5stBhOTkEGpHkLRMvuoUQuTCdqYjKtEw:1694448980289&q=pch+in+R&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjzmr2s-qKBAxUOGuwKHUdlAnMQ0pQJegQIDBAB&biw=960&bih=484&dpr=2#imgrc=lUw3nrgRKV8ynM
-plot(iron, diameter, pch=19, cex=2)
+plot(iron, diameter) # iron and diameter are arguments! # https://www.google.com/search?client=ubuntu-sn&hs=Ssn&sca_esv=564367827&channel=fs&sxsrf=AB5stBhOTkEGpHkLRMvuoUQuTCdqYjKtEw:1694448980289&q=pch+in+R&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjzmr2s-qKBAxUOGuwKHUdlAnMQ0pQJegQIDBAB&biw=960&bih=484&dpr=2#imgrc=lUw3nrgRKV8ynM
 plot(iron, diameter, pch=19, cex=2, col="red")
-
-# do you want info about functions?
 ?plot()
 
 # now we will use a package
@@ -39,21 +27,20 @@ im.list()
 # blue band
 b2 <- im.import("sentinel.dolomites.b2.tif") # b2 is the blue wavelength
 b2
-
 # green band
 b3 <- im.import("sentinel.dolomites.b3.tif") # b2 is the green wavelength
 b3
-
 # red band
-b4 <- im.import("sentinel.dolomites.b4.tif") # b2 is the green wavelength
+b4 <- im.import("sentinel.dolomites.b4.tif")
 b4
-
-# NIR band
-b8 <- im.import("sentinel.dolomites.b8.tif") # b2 is the green wavelength
+# NIR band infrared
+b8 <- im.import("sentinel.dolomites.b8.tif")
 b8
 
-plot(b2) # blue
 
+cl=colorRampPalette(c("black","DarkGrey","LightGray"))
+plot(b2, col=cl) # blue
+stacksent<-c(b2, b3, b4, b8)
 cl <- colorRampPalette(c("dark blue", "blue", "light blue")) (100) # 100 is the amount of colours in the gradient
 plot(b2, col=cl)
 
