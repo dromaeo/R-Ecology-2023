b3f1a1fb3be83f8ed95fab361a5a325092c2ad1e
dromaeo <146327980+dromaeo@users.noreply.github.com>
2023-11-09 17:27:29 +0100

    Update 05.spectralindeces.R

--- 05.spectralindeces.R
+++ 05.spectralindeces.R
@@ -35,23 +35,22 @@ plot(dvi1992, col=cl)
 
 # dvi2006
 dvi2006 = m2006[[1]] - m2006[[2]]
-dvi2006
-
-cl <- colorRampPalette(c("darkblue", "yellow", "red", "black"))(100) # specifying a color scheme
 plot(dvi2006, col=cl)
 
-# dvi by imageRy
-dvi1992i <- im.dvi(m1992, 1, 2)
-dvi2006i <- im.dvi(m2006, 1, 2)
-
+# since 2006 has less range, we normalize 0-1
 # ndvi
 ndvi1992 = (m1992[[1]] - m1992[[2]]) / (m1992[[1]] + m1992[[2]])
-ndvi2006 = (m2006[[1]] - m2006[[2]]) / (m2006[[1]] + m2006[[2]])
+#also ndvi1992 = dvi1992 /
+ndvi2006 = ndvi2006 / (m2006[[1]] + m2006[[2]])
 
 dev.off()
 par(mfrow=c(1,2))
 plot(ndvi1992, col=cl)
 plot(ndvi2006, col=cl)
+
+# dvi by imageRy
+dvi1992i <- im.dvi(m1992, 1, 2)
+dvi2006i <- im.dvi(m2006, 1, 2)
 
 # scientifically meaningful image for everyone!
 clvir <- colorRampPalette(c("violet", "dark blue", "blue", "green", "yellow"))(100) # specifying a color scheme
